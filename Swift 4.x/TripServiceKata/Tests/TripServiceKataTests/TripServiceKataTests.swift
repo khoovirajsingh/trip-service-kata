1d51808a1254b21618957c92a90f5d515a51d6ac
khoovis <khoovirajsingh.seewoonauth@mkodo.com>
2019-08-11 20:31:10 +0100

    Refactored test to use setup method

--- Swift 4.x/TripServiceKata/Tests/TripServiceKataTests/TripServiceKataTests.swift
+++ Swift 4.x/TripServiceKata/Tests/TripServiceKataTests/TripServiceKataTests.swift
@@ -2,9 +2,17 @@ import XCTest
 @testable import TripServiceKata
 
 class TripServiceKataTests: XCTestCase {
+    
+    var tripService: TripService!
+    var FRED: User!
+    
+    override func setUp() {
+        tripService = TestableTripService()
+        FRED = User()
+    }
+    
     func test_throws_user_not_logged_in_if_user_is_a_guest() {
         let tripService = TestableTripService()
-        let BOB = User()
-        XCTAssertThrowsError(try tripService.getTripsByUser(BOB))
+        XCTAssertThrowsError(try tripService.getTripsByUser(FRED))
     }
 }
